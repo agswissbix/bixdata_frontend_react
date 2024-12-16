493e99a866c14b2572573fe0e767cf5472b7a0cb
agswissbix <108857221+agswissbix@users.noreply.github.com>
2024-12-16 16:44:15 +0100

    .

--- bixdata_frontend_react/pages/home.tsx
+++ bixdata_frontend_react/pages/home.tsx
@@ -1,63 +1,30 @@
-import { useRouter } from 'next/router';
-import React, { useState, Suspense } from 'react';
-import withAuth from '../utils/withAuth';
-import axiosInstance from '../utils/axios';
-import ResponseProps from '../utils/responseInterface';
+import React, { useState } from 'react';
+import TestContent from '../components/testContent';
+import TestMenu from '../components/testMenu';
+import Sidebar from '../components/sidebar';
+import Navbar from '../components/navbar';
+import TableCardManager from '../components/tableCardManager';
 import '../app/globals.css';
-import NavbarComp from "@/components/navbar";
-import SidebarComp from "@/components/sidebar";
-import {Toaster} from "sonner";
-import LoadingComp from "@/components/loading";
-import dynamic from 'next/dynamic';
-import TableCardManager from '@/components/tableCardManager';
 
+// Main App Component
+const App: React.FC = () => {
+  const [selectedMenu, setSelectedMenu] = useState<string>('Home');
 
-const SettingsPage = dynamic(() => import("@/pages/settings"), {
-    loading: () => <LoadingComp /> // Aggiungi questo
-});
+  const handleMenuClick = (menuName: string): void => {
+    setSelectedMenu(menuName);
+  };
 
-const Home: React.FC<ResponseProps> = ({ response }) => {
-    const [currentComponent, setCurrentComponent] = useState<string>('');
-
-    // Funzione per cambiare il componente da caricare
-    const handleComponentChange = async (component: string) => {
-        try {
-            await axiosInstance.post('test_request/');
-            setCurrentComponent(component);
-        } catch (error) {
-            console.error('Errore durante il logout', error);
-        }
-    };
-
-    const router = useRouter();
-
-    const handleLogout = async () => {
-        try {
-            await axiosInstance.post('logout/');
-            router.push('/login');
-        } catch (error) {
-            console.error('Errore durante il logout', error);
-        }
-    };
-
-
-
-    return (
-        <div className="w-screen h-screen">
-            <Toaster richColors position="bottom-right" />
-
-            <NavbarComp />
-            <div className="w-full h-full flex">
-                <SidebarComp onChangeComponent={handleComponentChange} />
-                <div className=" relative w-full h-full bg-gray-100">
-                    <Suspense>
-                        {currentComponent === 'SettingsPage' && <SettingsPage />}
-                        {currentComponent === 'TableCardManager' && <TableCardManager  />}
-                    </Suspense>
-                </div>
-            </div>
-        </div>
-    );
+  return (
+    <div className="w-screen h-screen">
+      <Navbar />
+      <div className="w-full h-full flex">
+          <Sidebar onChangeComponent={handleMenuClick} />
+          <div className="relative w-full h-full bg-gray-100">
+            <TableCardManager tableid={selectedMenu} />
+          </div>
+      </div>
+    </div>
+  );
 };
 
-export default withAuth(Home, 'home');
+export default App;
