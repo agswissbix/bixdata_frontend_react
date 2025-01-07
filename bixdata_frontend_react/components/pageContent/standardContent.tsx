2120eee78e34339b536bbcdf8d3bb03dcbc50ada
agswissbix <108857221+agswissbix@users.noreply.github.com>
2025-01-07 16:51:45 +0100

    .

--- bixdata_frontend_react/components/pageContent/standardContent.tsx
+++ bixdata_frontend_react/components/pageContent/standardContent.tsx
@@ -29,7 +29,7 @@ const StandardContent: React.FC<ContentProps> = ({ tableid }) => {
       <div><RecordTabs tableid={tableid}></RecordTabs></div>
 
       <nav aria-label="Page navigation example" className="text-center">
-        <ul class="inline-flex text-sm">
+        <ul className="inline-flex text-sm">
           <li>
             <a href="#" className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Previous</a>
           </li>
